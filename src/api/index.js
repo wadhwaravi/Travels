4795de5913fa9f30016395c2d2ce86b95b36c0b9
wadhwaravi <117942469+wadhwaravi@users.noreply.github.com>
2024-09-09 23:03:39 +0530

    Apis

--- src/api/index.js
+++ src/api/index.js
@@ -1,24 +1,28 @@
-import axios from 'axios';
-
+import axios from "axios";
 
 export const getPlacesData = async (type, sw, ne) => {
   try {
-    const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
-      params: {
-        bl_latitude: sw.lat,
-        bl_longitude: sw.lng,
-        tr_longitude: ne.lng,
-        tr_latitude: ne.lat,
-      },
-      headers: {
-        // 'x-rapidapi-key': 'b758d7d8c5msh1cf65c6431e8612p15761ajsn5e7c0f9400ce',
-        'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
-      },
-    });
+    const {
+      data: { data },
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+          tr_latitude: ne.lat,
+        },
+        headers: {
+          "x-rapidapi-key":
+            "b758d7d8c5msh1cf65c6431e8612p15761ajsn5e7c0f9400ce",
+          "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+        },
+      }
+    );
 
     return data;
   } catch (error) {
     console.log(error);
   }
 };
-
